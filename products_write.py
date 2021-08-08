9378aa09887d7e5d962fd24615ab39928f18e72b
Billy Chiu <594billy@gmail.com>
2021-08-08 17:21:12 +0800

    read and write

--- products_write.py
+++ products_write.py
@@ -1,4 +1,15 @@
+#讀取檔案
 products = []
+with open('product.csv', 'r', encoding = 'utf-8-sig') as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue
+        else:
+            product, price = line.strip().split(',')
+            products.append([product, price])
+print(products)
+
+#輸入新資料
 while True:
     product = input('請輸入品名：')
     if product == 'q':
@@ -6,10 +17,12 @@ while True:
     price = input('請輸入價格：')
     products.append([product, price])
 
+#印出所有字料
 for each in products:
     print(each[0], '的價格是', each[1])
 
-with open('prodoct.csv', 'w', encoding = 'utf-8-sig') as f:
+#寫入資料
+with open('product.csv', 'w', encoding = 'utf-8-sig') as f:
     f.write('商品,價格\n')
     for each in products:
         f.write(each[0] + ',' + each[1] + '\n')
